3efb1d84b40c2b174ffadf73e7993e4221c604a0
Valentin Forster <121868573+ValentinForster@users.noreply.github.com>
2024-08-26 15:29:14 +0200

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -289,5 +289,5 @@ if uploaded_file is not None:
     sus = sus.sort_values(by='pct_suspicious_models', ascending=False)
 
     st.write(sus)
-    st.title("Explanation")
+    st.markdown("""**Explanation**""")
     st.write("This table show the percentage of suspicious models for each Manufacturer in the dataset.")
