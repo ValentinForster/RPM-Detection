47e3efbfc07f6ee210e6160448ecda61d5fcb90a
Valentin Forster <121868573+ValentinForster@users.noreply.github.com>
2024-09-18 10:42:31 +0200

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -332,12 +332,12 @@ if uploaded_file is not None:
     # Define the filtered version of the DataFrame
     if view_option == "Suspicious Manufacturers only":
         st.dataframe(sus.head(2), hide_index=True)
-        st.markdown("""**Explanation**""")
-        st.write("This table shows only those manufacturers, which are likely to use RPM. They have 5 or more suspicious products and over 40 % of their products are suspicious.")
+        st.markdown("""**How to interpret results**""")
+        st.write("This table shows only those manufacturers from the dataset, which are likely using RPM. They have 5 or more suspicious products and over 40 % of their products are suspicious.")
     else:
         # Display the unfiltered DataFrame
         st.dataframe(sus, hide_index=True)
-        st.markdown("""**Explanation**""")
-        st.write("This table shows the number as well as the percentage of suspicious models for each Manufacturer in the dataset.")
+        st.markdown("""**How to interpret results**""")
+        st.write("This table shows the number as well as the percentage of suspicious models for each Manufacturer in the dataset. Please note that a high pct_suspicious_models with a small sample size (e.g. < 5 suspicious models) is meaningless.")
   
     #st.dataframe(sus, hide_index=True)
