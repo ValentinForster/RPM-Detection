0756d16ba436ea9f96544cb8279bbd34c7d71437
Valentin Forster <121868573+ValentinForster@users.noreply.github.com>
2024-12-05 14:50:35 +0100

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -14,7 +14,7 @@ import os
 
 st.title("RPM detection tool")
 
-st.write("File exists:", os.path.exists("Scraper.ipynb"))
+st.write("File exists:", os.path.exists("Freezers.csv"))
 
 st.write("""
 This tool will find indications of [Resale Price Maintenance (RPM)](https://en.wikipedia.org/wiki/Resale_price_maintenance) in price data. 
